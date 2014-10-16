9497e198e72d4e7b424a6c43bb67cc722bafe50e
cmuzzin <cmuzzin@uark.edu>
2014-10-16 15:50:51 -0500

    just a test

--- app/src/androidTest/java/mobile/csce/uark/flash/ApplicationTest.java
+++ app/src/androidTest/java/mobile/csce/uark/flash/ApplicationTest.java
@@ -10,4 +10,12 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+
+    public void chriscommit()
+    {
+       //test
+    }
+
+
 }
