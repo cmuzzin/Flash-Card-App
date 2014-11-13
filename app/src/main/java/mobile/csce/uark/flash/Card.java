26690c6ee2780db64dcd4562297dd4339c928e78
Jonathan <jstewart8301@me.com>
2014-11-12 21:28:59 -0600

    Fixed the CreateDeack added the Card Class added the two Schema Images

--- app/src/main/java/mobile/csce/uark/flash/Card.java
+++ app/src/main/java/mobile/csce/uark/flash/Card.java
@@ -4,8 +4,45 @@ package mobile.csce.uark.flash;
  * Created by Jonathan on 11/12/14.
  */
 public class Card {
-    String FrontSide;
-    String BackSide;
-    Integer Number;
-    Integer ID;
+
+
+    private String FrontSide;
+    private String BackSide;
+    private Integer Number;
+    public Integer ID;
+    public Integer DeckID;
+
+    public Card(String frontSide, Integer number, Integer ID, Integer deckID) {
+        FrontSide = frontSide;
+        Number = number;
+        this.ID = ID;
+        DeckID = deckID;
+    }
+
+    public String getFrontSide() {
+        return FrontSide;
+    }
+
+    public void setFrontSide(String frontSide) {
+        FrontSide = frontSide;
+    }
+
+    public String getBackSide() {
+        return BackSide;
+    }
+
+    public void setBackSide(String backSide) {
+        BackSide = backSide;
+    }
+    public Integer getNumber() {
+        return Number;
+    }
+
+    public void setNumber(Integer number) {
+        Number = number;
+    }
+
+
 }
+
+
