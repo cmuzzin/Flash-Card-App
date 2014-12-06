d6a4e855ab4523ec633c1d5bba4d3c0b5193d453
cmuzzin <cmuzzin@uark.edu>
2014-12-06 01:20:34 -0600

    error checking for saving a deck

--- app/src/main/java/mobile/csce/uark/flash/CreateDeck.java
+++ app/src/main/java/mobile/csce/uark/flash/CreateDeck.java
@@ -3,21 +3,27 @@ package mobile.csce.uark.flash;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
 
 
 public class CreateDeck extends Activity {
+    private Toast toast = null;
 
     //private Button savedeck;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_deck);
+        toast = Toast.makeText(getApplicationContext(), "",Toast.LENGTH_LONG);
+
     }
 
 
@@ -29,17 +35,41 @@ public class CreateDeck extends Activity {
     }
 
     public void savedeck(View view){
-        Deck createdeckitem = new Deck(0,((TextView) findViewById(R.id.editText)).getText().toString());
-        FlashDatabase db = new FlashDatabase(this);
-        db.open();
-        db.InsertDeck(createdeckitem);
-        db.close();
-        Intent i = new Intent();
-        setResult(RESULT_OK,i);
 
-        //createdeckitem.setDeckname(((TextView) findViewById(R.id.editText)).getText().toString());
+        Deck createdeckitem = new Deck();
+        createdeckitem.setDeckname(((EditText) findViewById(R.id.editText)).getText().toString());
+        Intent intent = new Intent();
+        intent.putExtra("nameid", (Serializable)createdeckitem);
+        TextView deckname = (TextView) findViewById(R.id.editText);
+        String dn = deckname.getText().toString();
+        System.out.println("Value of my string" + dn);
+        String temp = dn.trim();
+       //if ((dn.isEmpty()) || (dn.matches("[a-zA-Z1-9]+") == false) || (dn.matches(".*\\w.*")))
+        if (temp.equals(""))
 
-        finish();
+        {
+            toast.setText("Deck needs a name");
+            toast.setGravity(Gravity.TOP| Gravity.LEFT, 0 ,500);
+            toast.show();
+            setResult(RESULT_CANCELED,intent);
+
+
+            //setResult(RESULT_OK, intent);
+            //finish();
+        }
+
+        else
+        {
+
+            setResult(RESULT_OK, intent);
+            toast.cancel();
+            finish();
+        }
+
+
+
+        //setResult(RESULT_OK, intent);
+        //finish();
 
     }
 
